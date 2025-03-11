daee0d02ca5fff233284859d9bf06c4273236707
Gehad Baleegh <93608888+jihad696@users.noreply.github.com>
2025-03-11 13:11:10 +0200

    Add files via upload

---
+++ Home/src/home/Home.java
@@ -0,0 +1,17 @@
+
+package home;
+
+/**
+ *
+ * @author Gehad
+ */
+public class Home {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
